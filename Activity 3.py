a0bc8081fb34b30cee2e7405acd263ce174e31db
Hiyaa2013 <hiyaah.shimpi@gmail.com>
2025-11-12 19:53:33 +0530

    Add files via upload

--- Activity 3.py
+++ Activity 3.py
@@ -1,36 +1,10 @@
-print("Enter  marks obtained in 5 subjects....")
-marks1=int(input())
-marks2=int(input())
-marks3=int(input())
-marks4=int(input())
-marks5=int(input())
-
-tot=marks1+marks2+marks3+marks4+marks5
-avg=tot/5 
-
-if avg>=91 and avg<=100:
-    print("your grade is O+")
-elif avg>=81 and avg<=91:
-    print("your grade is A++")
-elif avg>=71 and avg<=81:
-    print("your grade  A+")
-elif avg>=61 and avg<=71:
-    print("your grade  A")
-elif avg>=51 and avg<=61:
-    print("your grade  B+")
-elif avg>=41 and avg<=51:
-    print("your grade  B")
-elif avg>=31 and avg<=41:
-    print("your grade  C+")
-elif avg>=21 and avg<=31:
-    print("your grade  C")
-elif avg>=11 and avg<=21:
-    print("your grade  C")
-elif avg>=6 and avg<=11:
-    print("your grade  D+")
-elif avg>=0 and avg<=6:
-    print("your grade  F")
-else:
-    print(" 404!! ERROR INVAILD INPUT PAGE NOT FOUND")
-    
-
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+sum=mean1*total_number
+print("the sum of 40 numbers:",sum)
+num2=sum-((wrong_number)-(correct_number))
+print("sum-((wrong_number)-(correct_number))",num2)
+mean2=num2/total_number
+print(mean2)
